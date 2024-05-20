86af23ec03716077c24bc15fdc5401533561da9c
Matheus Lima <matheus.lima.ext@britvic.com.br>
2024-05-20 18:31:00 -0300

    init: starting the development

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import GameView from './views/GameView.vue';
+</script>
+
+<template>
+  <GameView />
+</template>
+
+<style >
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import GameView from '../views/GameView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'game',
+      component: GameView
+    },
+    {
+      path: '/game',
+      name: 'game2',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/GameView.vue')
+    }
+  ]
+})
+
+export default router
