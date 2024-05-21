8de564bf718578de324367c1f29317f5ac2b0dcf
Matheus Lima <matheus.lima.ext@britvic.com.br>
2024-05-21 01:14:36 -0300

    feat: componente com as letras

---
+++ src/components/Hexagon.vue
@@ -0,0 +1,37 @@
+<template>
+    <div v-for="(letter, index) in letters" :key="index" class="hexagon">
+        <span>{{ letter }}</span>
+    </div>
+</template>
+
+
+<script setup>
+    const props = defineProps(['letters'])
+</script>
+
+<style scoped>
+
+
+.hexagon {
+  width: 70px;
+  height: 81px;
+  background-color: #e4eaf3;
+  margin: -8px 0px -8px 5px;
+  clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+  color: black;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px; /* Adjust font size as needed */
+  position: relative;
+}
+.hexagon:nth-child(1) { grid-column: 2; grid-row: 1; }
+.hexagon:nth-child(2) { grid-column: 4; grid-row: 1; }
+.hexagon:nth-child(3) { grid-column: 1; grid-row: 2; }
+.hexagon:nth-child(4) { grid-column: 3; grid-row: 2; }
+.hexagon:nth-child(5) { grid-column: 5; grid-row: 2; }
+.hexagon:nth-child(6) { grid-column: 2; grid-row: 3; }
+.hexagon:nth-child(7) { grid-column: 4; grid-row: 3; }
+
+</style>
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,39 +1,35 @@
 <template>
   <div class="about w-full h-full flex flex-col md:flex-row bg-green-300">
-      <!-- Left Column -->
       <div class="left-column w-full md:w-1/2 flex flex-col justify-center items-center p-4">
-        <div class="flower">
-            <div v-for="(letter, index) in todaysLetters" :key="index" class="hexagon">
-              <span>{{ letter }}</span>
-            </div>
-
-        </div>
-        <div class="w-full p-5 mx-4 relative">
-          <input type="text" class="invisible-input font-xl text-2xl text-white uppercase font-extrabold w-full bg-transparent border-none outline-none text-black placeholder-gray-500">
-        </div>
-
+          <div class="flower">
+            <Hexagon :letters="todaysLetters"/>
+          </div>
+          <div class="w-full p-5 mx-4 relative">
+            <input 
+              type="text" 
+              class="invisible-input text-4xl text-white uppercase font-extrabold w-full bg-transparent border-none outline-none text-black placeholder-gray-500"
+              v-model="word"
+              @keyup.enter="submitWord"
+          >
+          </div>
+        
       </div>
-      <!-- Right Column -->
       <div class="right-column w-full md:w-1/2 p-4 justify-center">
-          <!-- Table Component Placeholder -->
           <table class="table-auto w-full bg-white shadow-md rounded-lg">
               <thead>
                   <tr>
-                      <th class="px-4 py-2">Header 1</th>
-                      <th class="px-4 py-2">Header 2</th>
-                      <th class="px-4 py-2">Header 3</th>
+                      <th class="px-4 py-2">Palavra</th>
+                      <th class="px-4 py-2">Pontuação</th>
                   </tr>
               </thead>
               <tbody>
                   <tr>
-                      <td class="border px-4 py-2">Data 1</td>
-                      <td class="border px-4 py-2">Data 2</td>
-                      <td class="border px-4 py-2">Data 3</td>
+                      <td class="border px-4 py-2">Teste</td>
+                      <td class="border px-4 py-2">100</td>
                   </tr>
                   <tr>
-                      <td class="border px-4 py-2">Data 4</td>
-                      <td class="border px-4 py-2">Data 5</td>
-                      <td class="border px-4 py-2">Data 6</td>
+                      <td class="border px-4 py-2">Teste</td>
+                      <td class="border px-4 py-2">50</td>
                   </tr>
               </tbody>
           </table>
@@ -43,48 +39,26 @@
 
 <script setup>
   import { ref } from 'vue'
+  import Hexagon from '../components/Hexagon.vue';
 
   const todaysLetters = ref(['A','B', 'C', 'D', 'E', 'F', 'G']);
-
+  const word = ref('');
+  
+  const submitWord = (e) =>  {
+    console.log(word.value);
+  }
 </script>
 
   
 <style>
 
 .flower {
-/*   display: flex;
-  flex-direction: column;
-  align-items: center;
- */
   display: grid;
   grid-template-columns: repeat(5, 38px);
   grid-template-rows: repeat(3, 68px);
   
   justify-content: center;
 }
-
-
-.hexagon {
-  width: 70px;
-  height: 81px;
-  background-color: #e4eaf3;
-  margin: -8px 0px -8px 5px;
-  clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
-  color: black;
-  font-weight: bold;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 24px; /* Adjust font size as needed */
-  position: relative;
-}
-.hexagon:nth-child(1) { grid-column: 2; grid-row: 1; }
-.hexagon:nth-child(2) { grid-column: 4; grid-row: 1; }
-.hexagon:nth-child(3) { grid-column: 1; grid-row: 2; }
-.hexagon:nth-child(4) { grid-column: 3; grid-row: 2; }
-.hexagon:nth-child(5) { grid-column: 5; grid-row: 2; }
-.hexagon:nth-child(6) { grid-column: 2; grid-row: 3; }
-.hexagon:nth-child(7) { grid-column: 4; grid-row: 3; }
 
 </style>
   
