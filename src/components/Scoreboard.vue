b334c630ac7a3ca6199048ef45ea9f511c899593
Matheus Lima <matheus.lima.ext@britvic.com.br>
2024-05-22 01:33:01 -0300

    wip: validation of the word

---
+++ src/components/Scoreboard.vue
@@ -0,0 +1,21 @@
+<template>
+    <table class="table-auto w-full bg-white shadow-md rounded-lg">
+        <thead>
+            <tr>
+                <th class="px-4 py-2">Palavra</th>
+                <th class="px-4 py-2">Pontuação</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="row in score">
+                <td class="border px-4 py-2">{{ row.word }}</td>
+                <td class="border px-4 py-2">{{ row.score }}</td>
+            </tr>
+            
+        </tbody>
+    </table>
+</template>
+
+<script setup>
+    const props = defineProps(['score'])
+</script>
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,39 +1,26 @@
 <template>
   <div class="about w-full h-full flex flex-col md:flex-row bg-green-300">
-      <div class="left-column w-full md:w-1/2 flex flex-col justify-center items-center p-4">
-          <div class="flower">
-            <Hexagon :letters="todaysLetters"/>
+      <div class="left-column w-full md:w-1/2 flex flex-col justify-center  p-4">
+          <div class="flex justify-center h-2/6">
+            <h1 class="text-6xl">Green Grammar</h1>
           </div>
-          <div class="w-full p-5 mx-4 relative">
-            <input 
-              type="text" 
-              class="invisible-input text-4xl text-white uppercase font-extrabold w-full bg-transparent border-none outline-none text-black placeholder-gray-500"
-              v-model="word"
-              @keyup.enter="submitWord"
-          >
-          </div>
+          <div class="h-4/6">
+            <div class="flower">
+              <Hexagon :letters="todaysLetters"/>
+            </div>
+            <div class="w-full p-5 mx-4 relative">
+              <input 
+                type="text" 
+                class="invisible-input text-4xl text-white uppercase font-extrabold w-full bg-transparent border-none outline-none text-black placeholder-gray-500"
+                v-model="word"
+                @keyup.enter="submitWord"
+            >
+            </div>
+        </div>
         
       </div>
-      <span>{{ isWordLong && isWordValid && haveCentralLetter ? 'Válida' : 'inválida' }}</span>
       <div class="right-column w-full md:w-1/2 p-4 justify-center">
-          <table class="table-auto w-full bg-white shadow-md rounded-lg">
-              <thead>
-                  <tr>
-                      <th class="px-4 py-2">Palavra</th>
-                      <th class="px-4 py-2">Pontuação</th>
-                  </tr>
-              </thead>
-              <tbody>
-                  <tr>
-                      <td class="border px-4 py-2">Teste</td>
-                      <td class="border px-4 py-2">100</td>
-                  </tr>
-                  <tr>
-                      <td class="border px-4 py-2">Teste</td>
-                      <td class="border px-4 py-2">50</td>
-                  </tr>
-              </tbody>
-          </table>
+        <Scoreboard :score="score"/>
       </div>
   </div>
 </template>
@@ -41,24 +28,46 @@
 <script setup>
   import { ref, computed } from 'vue'
   import Hexagon from '../components/Hexagon.vue';
+  import Scoreboard from '../components/Scoreboard.vue';
 
   const todaysLetters = ref(['A','B', 'C', 'D', 'E', 'F', 'G']);
   const word = ref('');
-  
+  const score = ref([
+    {
+      word: "test",
+      score: 10
+    },
+    {
+      word: "test 2",
+      score: 20
+    },
+  ]);
   const submitWord = (e) =>  {
-    console.log(word.value);
+    if (isWordValid.value) {
+
+      score.value.push({
+        "word": word.value,
+        "score": 100
+      });
+    }
+    word.value = '';
   }
 
   const isWordLong = computed(() => {
     return word.value.length > 3;
-  })
-  const isWordValid = computed(() => {
+  });
+  const isWordValidForToday = computed(() => {
     return word.value.split('').every(letter => todaysLetters.value.includes(letter.toUpperCase()));
-  })
+  });
 
   const haveCentralLetter = computed(() => {
     return word.value.toUpperCase().includes(todaysLetters.value[3]);
-  })
+  });
+
+  const isWordValid = computed(() => {
+    return isWordLong.value && isWordValidForToday.value && haveCentralLetter.value;
+  });
+ 
 
 </script>
 
